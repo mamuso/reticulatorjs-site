b91fd95a1dc7656378b1626a2ee5a8fef11667e6
“mamuso” <mamuso@mamuso.net>
2009-08-31 00:17:08 +0200

    examples

--- reticulatorsite.rb
+++ reticulatorsite.rb
@@ -1,6 +1,31 @@
 require 'rubygems'
 require 'sinatra'
+require 'lib/sinatra/content_for'
 
 get '/' do
   erb :home
+end
+
+get '/example_simplegrid' do
+  erb :example_simplegrid
+end
+
+get '/example_customgrid_extraguides' do
+  erb :example_customgrid_extraguides
+end
+
+get '/example_twogrids' do
+  erb :example_twogrids
+end
+
+get '/example_emptygrid' do
+  erb :example_emptygrid
+end
+
+get '/example_horizontal' do
+  erb :example_horizontal
+end
+
+get '/example_vertical' do
+  erb :example_vertical
 end
