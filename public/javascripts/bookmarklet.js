ba2f98d7066a94ef131ab9a1299f7daf7482b01c
“mamuso” <mamuso@mamuso.net>
2009-09-01 01:11:33 +0200

    compact

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -1,102 +1,100 @@
 
 var custombookmarkletfunction = ""+
-"  if(window.Reticulator) { return; }"+
-"  var d = document "+
-"  ,s = d.createElement('script') "+
-"  ,wo = window.onload "+
-"  ,go = function () { "+
-"    new Reticulator({"+
-"      align: rtalign,"+
-"      width: rtwidth,"+
-"      offset: rtoffset,"+
-"      columns: rtcolumns,"+
-"      gutter: rtgutter,"+
-"      color: rtcolor,"+
-"      opacity: rtopacity,"+
-"      zindex: rtzindex"+
-"    });"+
-"    window.onload = wo || null; "+
-"   }; "+
-"  s.src = 'http://github.com/mamuso/reticulatorjs/raw/master/reticulator.js?x='+(Math.random()); "+
-"  if (null === s.onreadystatechange) {"+
-"    s.onreadystatechange = function () { "+
-"      if (s.readyState == 'complete') "+
-"      go(); "+
-"    }; "+
-"  } else {"+
-"    s.onload = go; "+
-"    d.body.appendChild(s); "+
-"  }";
+"if(window.Reticulator) { return; }"+
+"var d = document "+
+",s = d.createElement('script') "+
+",wo = window.onload "+
+",go = function () { "+
+"new Reticulator({"+
+"align: rtalign,"+
+"width: rtwidth,"+
+"offset: rtoffset,"+
+"columns: rtcolumns,"+
+"gutter: rtgutter,"+
+"color: rtcolor,"+
+"opacity: rtopacity,"+
+"zindex: rtzindex"+
+"});"+
+"window.onload = wo || null; "+
+" }; "+
+"s.src = 'http://github.com/mamuso/reticulatorjs/raw/master/reticulator.js?x='+(Math.random()); "+
+"if (null === s.onreadystatechange) {"+
+"s.onreadystatechange = function () { "+
+"if (s.readyState == 'complete') "+
+"go(); "+
+"}; "+
+"} else {"+
+"s.onload = go; "+
+"d.body.appendChild(s); "+
+"}";
 
 var bookmarkletfunction = ""+
-"  if(window.Reticulator) { return; }"+
-"  var d = document "+
-"  ,s = d.createElement('script') "+
-"  ,sr = d.createElement('script') "+
-"  ,wo = window.onload "+
-"  ,w = window "+
-"  ,a = function () {"+
-"   top.wref = window.open('', 'RTconfig','menubar=0,toolbar=0,resizable=0,status=1,width=220,height=400');"+
+"if(window.Reticulator) { return; }"+
+"var d = document "+
+",s = d.createElement('script') "+
+",sr = d.createElement('script') "+
+",wo = window.onload "+
+",w = window "+
+",a = function () {"+
+" top.wref = window.open('', 'RTconfig','menubar=0,toolbar=0,resizable=0,status=1,width=220,height=400');"+
 "}"+
-"  ,go = function () { "+
-"    new RToptions();"+
-"    window.onload = wo || null; "+
-"   }; "+
-"     if(/Firefox/.test(navigator.userAgent)){setTimeout(a,1);}else{a();}"+
-"  s.src = 'http://github.com/mamuso/reticulatorjs/raw/master/reticulator.js?x='+(Math.random()); "+
-"  sr.src = 'http://reticulatorjs.mamuso.net/javascripts/bookmarkletoptions.js?x='+(Math.random()); "+
-"  if (null === s.onreadystatechange) {"+
-"    s.onreadystatechange = function () { "+
-"      if (s.readyState == 'complete') "+
-"      go(); "+
-"    }; "+
-"  } else {"+
-"    d.body.appendChild(sr); "+
-"    d.body.appendChild(s); "+
-"    s.onload = go; "+
-"  }";
+",go = function () { "+
+"new RToptions();"+
+"window.onload = wo || null; "+
+" }; "+
+" if(/Firefox/.test(navigator.userAgent)){setTimeout(a,1);}else{a();}"+
+"s.src = 'http://github.com/mamuso/reticulatorjs/raw/master/reticulator.js?x='+(Math.random()); "+
+"sr.src = 'http://reticulatorjs.mamuso.net/javascripts/bookmarkletoptions.js?x='+(Math.random()); "+
+"if (null === s.onreadystatechange) {"+
+"s.onreadystatechange = function () { "+
+"if (s.readyState == 'complete') "+
+"go(); "+
+"}; "+
+"} else {"+
+"d.body.appendChild(sr); "+
+"d.body.appendChild(s); "+
+"s.onload = go; "+
+"}";
 
 
 var setBookmarkletForm = function () {
-  document.getElementById("bookmarkletform").onsubmit = function(){
-    setBookmarklet();
-    return false;
-  }
-  
-  setBookmarklet();
+document.getElementById("bookmarkletform").onsubmit = function(){
+setBookmarklet();
+return false;
+}
+
+setBookmarklet();
 };
 
 var setBookmarklet = function() {
-  document.getElementById("reticulatorjsbookmarklet").href = "javascript:(function(){"+
-    bookmarkletfunction +
-  "})();"
+document.getElementById("reticulatorjsbookmarklet").href = "javascript:(function(){"+
+bookmarkletfunction +
+"})();"
 
-  document.getElementById("customreticulatorjsbookmarklet").href = "javascript:(function(){"+
-    "var rtalign, rtwidth, rtoffset, rtcolumns, rtgutter, rtcolor, rtopacity, rtzindex;"+
-    "rtalign = '" + document.getElementById("grid_align").value + "';"+
-    "rtwidth = " + document.getElementById("grid_width").value + ";"+
-    "rtoffset = " + document.getElementById("grid_offset").value + ";"+
-    "rtcolumns = " + document.getElementById("grid_columns").value + ";"+
-    "rtgutter = " + document.getElementById("grid_gutter").value + ";"+
-    "rtcolor = '" + document.getElementById("grid_color").value + "';"+
-    "rtopacity = " + document.getElementById("grid_opacity").value + ";"+
-    "rtzindex = " + document.getElementById("grid_zindex").value + ";"+
-    custombookmarkletfunction +
-  "})();"
-  document.getElementById("customreticulatorjsbookmarklet").innerHTML = document.getElementById("grid_width").value + "/" + document.getElementById("grid_columns").value + "/" + document.getElementById("grid_gutter").value + " grid";
-  document.getElementById("updatedbookmarklet").innerHTML = "values: " + 
-    document.getElementById("grid_align").value + " | " + 
-    document.getElementById("grid_width").value + " | " + 
-    document.getElementById("grid_offset").value + " | " + 
-    document.getElementById("grid_columns").value + " | " + 
-    document.getElementById("grid_gutter").value + " | " + 
-    document.getElementById("grid_color").value + " | " + 
-    document.getElementById("grid_opacity").value + " | " + 
-    document.getElementById("grid_zindex").value; 
-
+document.getElementById("customreticulatorjsbookmarklet").href = "javascript:(function(){"+
+"var rtalign, rtwidth, rtoffset, rtcolumns, rtgutter, rtcolor, rtopacity, rtzindex;"+
+"rtalign = '" + document.getElementById("grid_align").value + "';"+
+"rtwidth = " + document.getElementById("grid_width").value + ";"+
+"rtoffset = " + document.getElementById("grid_offset").value + ";"+
+"rtcolumns = " + document.getElementById("grid_columns").value + ";"+
+"rtgutter = " + document.getElementById("grid_gutter").value + ";"+
+"rtcolor = '" + document.getElementById("grid_color").value + "';"+
+"rtopacity = " + document.getElementById("grid_opacity").value + ";"+
+"rtzindex = " + document.getElementById("grid_zindex").value + ";"+
+custombookmarkletfunction +
+"})();"
+document.getElementById("customreticulatorjsbookmarklet").innerHTML = document.getElementById("grid_width").value + "/" + document.getElementById("grid_columns").value + "/" + document.getElementById("grid_gutter").value + " grid";
+document.getElementById("updatedbookmarklet").innerHTML = "values: " + 
+document.getElementById("grid_align").value + " | " + 
+document.getElementById("grid_width").value + " | " + 
+document.getElementById("grid_offset").value + " | " + 
+document.getElementById("grid_columns").value + " | " + 
+document.getElementById("grid_gutter").value + " | " + 
+document.getElementById("grid_color").value + " | " + 
+document.getElementById("grid_opacity").value + " | " + 
+document.getElementById("grid_zindex").value; 
 };
 
-
 window.onload = function() {
-  setBookmarkletForm();
+setBookmarkletForm();
 }
--- public/javascripts/bookmarkletoptions.js
+++ public/javascripts/bookmarkletoptions.js
@@ -1,5 +1,5 @@
 var windowoptions = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"'+
-'  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'+
+'"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'+
 '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">'+
 '<head>'+
 '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'+
@@ -8,81 +8,80 @@ var windowoptions = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"'+
 '</head>'+
 '<body>'+
 '<form action="#" method="post" accept-charset="utf-8" id="RTform">'+
-'  <div id="configurereticulator">'+
+'<div id="configurereticulator">'+
 ''+
-'    <h2>setup your grid</h2>'+
-'      <p>'+
-'        <label for="grid_align">align</label>'+
-'        <select id="grid_align" name="grid_align">'+
-'          <option value="center">center</option>'+
-'          <option value="left">left</option>'+
-'          <option value="right">right</option>'+
-'        </select>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_width">width</label>'+
-'        <input type="text" id="grid_width" value="951" name="grid_width"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_offset">offset</label>'+
-'        <input type="text" id="grid_offset" value="0" name="grid_offset"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_columns">columns</label>'+
-'        <input type="text" id="grid_columns" value="16" name="grid_columns"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_gutter">gutter</label>'+
-'        <input type="text" id="grid_gutter" value="9" name="grid_gutter"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_color">color</label>'+
-'        <input type="text" id="grid_color" value="#00FF00" name="grid_color"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_opacity">opacity</label>'+
-'        <input type="text" id="grid_opacity" value="0.5" name="grid_opacity"/>'+
-'      </p>'+
-'      <p>'+
-'        <label for="grid_zindex">zindex</label>'+
-'        <input type="text" id="grid_zindex" value="9000000" name="grid_zindex"/>'+
-'      </p>'+
+'<h2>setup your grid</h2>'+
+'<p>'+
+'<label for="grid_align">align</label>'+
+'<select id="grid_align" name="grid_align">'+
+'<option value="center">center</option>'+
+'<option value="left">left</option>'+
+'<option value="right">right</option>'+
+'</select>'+
+'</p>'+
+'<p>'+
+'<label for="grid_width">width</label>'+
+'<input type="text" id="grid_width" value="951" name="grid_width"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_offset">offset</label>'+
+'<input type="text" id="grid_offset" value="0" name="grid_offset"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_columns">columns</label>'+
+'<input type="text" id="grid_columns" value="16" name="grid_columns"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_gutter">gutter</label>'+
+'<input type="text" id="grid_gutter" value="9" name="grid_gutter"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_color">color</label>'+
+'<input type="text" id="grid_color" value="#00FF00" name="grid_color"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_opacity">opacity</label>'+
+'<input type="text" id="grid_opacity" value="0.5" name="grid_opacity"/>'+
+'</p>'+
+'<p>'+
+'<label for="grid_zindex">zindex</label>'+
+'<input type="text" id="grid_zindex" value="9000000" name="grid_zindex"/>'+
+'</p>'+
 ''+
-'      <p class="actions">'+
-'        <button type="submit" id="RTform_button">get a grid!</button>'+
-'      </p>'+
-'  </div>'+
+'<p class="actions">'+
+'<button type="submit" id="RTform_button">get a grid!</button>'+
+'</p>'+
+'</div>'+
 '</form>'+
 '</body>'+
 '</html>';
 
-
 var RToptions = function () {
-    top.wref.document.writeln(windowoptions);
-    top.wref.document.getElementById("RTform").onsubmit = function(){
-      top.wref.opener.eval(""+
-        "var rtalign, rtwidth, rtoffset, rtcolumns, rtgutter, rtcolor, rtopacity, rtzindex;"+
-        "rtalign = '" + top.wref.document.getElementById("grid_align").value + "';"+
-        "rtwidth = " + top.wref.document.getElementById("grid_width").value + ";"+
-        "rtoffset = " + top.wref.document.getElementById("grid_offset").value + ";"+
-        "rtcolumns = " + top.wref.document.getElementById("grid_columns").value + ";"+
-        "rtgutter = " + top.wref.document.getElementById("grid_gutter").value + ";"+
-        "rtcolor = '" + top.wref.document.getElementById("grid_color").value + "';"+
-        "rtopacity = " + top.wref.document.getElementById("grid_opacity").value + ";"+
-        "rtzindex = " + top.wref.document.getElementById("grid_zindex").value + ";"+
-        "new Reticulator({"+
-        "  align: rtalign,"+
-        "  width: rtwidth,"+
-        "  offset: rtoffset,"+
-        "  columns: rtcolumns,"+
-        "  gutter: rtgutter,"+
-        "  color: rtcolor,"+
-        "  opacity: rtopacity,"+
-        "  zindex: rtzindex"+
-        "});"
-      );
-      top.wref.opener.focus();
-      top.wref.close();
-      return false;
-    }
+top.wref.document.writeln(windowoptions);
+top.wref.document.getElementById("RTform").onsubmit = function(){
+top.wref.opener.eval(""+
+"var rtalign, rtwidth, rtoffset, rtcolumns, rtgutter, rtcolor, rtopacity, rtzindex;"+
+"rtalign = '" + top.wref.document.getElementById("grid_align").value + "';"+
+"rtwidth = " + top.wref.document.getElementById("grid_width").value + ";"+
+"rtoffset = " + top.wref.document.getElementById("grid_offset").value + ";"+
+"rtcolumns = " + top.wref.document.getElementById("grid_columns").value + ";"+
+"rtgutter = " + top.wref.document.getElementById("grid_gutter").value + ";"+
+"rtcolor = '" + top.wref.document.getElementById("grid_color").value + "';"+
+"rtopacity = " + top.wref.document.getElementById("grid_opacity").value + ";"+
+"rtzindex = " + top.wref.document.getElementById("grid_zindex").value + ";"+
+"new Reticulator({"+
+"align: rtalign,"+
+"width: rtwidth,"+
+"offset: rtoffset,"+
+"columns: rtcolumns,"+
+"gutter: rtgutter,"+
+"color: rtcolor,"+
+"opacity: rtopacity,"+
+"zindex: rtzindex"+
+"});"
+);
+top.wref.opener.focus();
+top.wref.close();
+return false;
+}
 };
